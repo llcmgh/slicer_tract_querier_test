5b8fe0ff328222773a3445b30dc2a681d13396ea
llcmgh <lichenl@nmr.mgh.harvard.edu>
2014-03-06 16:30:17 -0500

    Update __init__.py

--- tract_querier/__init__.py
+++ tract_querier/__init__.py
@@ -5,12 +5,15 @@ from tract_label_indices import *
 from shell import *
 import tractography
 
-
-default_queries_folder = os.path.abspath(os.path.join(
-    os.path.dirname(__file__),
-    '..', '..', '..', '..',
-    'tract_querier', 'queries'
-))
+import inspect,os
+fullpath=os.path.join(os.getcwd(),inspect.getfile(inspect.currentframe()))
+default_queries_folder, filename = os.path.split(fullpath)
+default_queries_folder=default_queries_folder+'/data'
+#default_queries_folder = os.path.abspath(os.path.join(
+#    os.path.dirname(__file__),
+#    '..', '..', '..', '..',
+#    'tract_querier', 'queries'
+#))
 
 #import tract_metrics
 
